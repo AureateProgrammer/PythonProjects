138614a52b0979d55bed6c9c2239208b5d9414d6
AureteProgrammer <CodingBlakeMorris@gmail.com>
2026-09-07 11:04:42 -0400

    importing flask into project

--- Projects/ProjectOne.py
+++ Projects/ProjectOne.py
@@ -1,8 +1,18 @@
 import time 
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello from Flask!"
+
+if __name__ == '__main__':
+    app.run(port=5000)
 
 def main():
     print('Give me a second printing time.....',)
     time.sleep(3)
     print(time.asctime())
-    
 main()
+
